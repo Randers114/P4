0c567b97b95b447f4c003e8bf5ef1ceaca89f5a2
Randers114 <srande15@student.aau.dk>
2017-04-05 09:32:47 +0200

    Main og sourceControl

--- compiler/src/Main.java
+++ compiler/src/Main.java
@@ -1,5 +1,6 @@
 import abstractSyntaxTree.AstBuild;
 import abstractSyntaxTree.nodes.ProgramNode;
+import kotlin.SinceKotlin;
 import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.TokenStream;
 import prettyPrint.PrettyPrint;
@@ -10,18 +11,13 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-<<<<<<< HEAD
         Scanner scanner = new Scanner(System.in);
         String InputPath = scanner.nextLine();
         
         org.antlr.v4.runtime.CharStream charStream = new ANTLRFileStream(InputPath);
-        FinalGrammarLexer lexer = new FinalGrammarLexer(charStream);
-=======
-        org.antlr.v4.runtime.CharStream charStream = new ANTLRFileStream("C:\\Users\\Stefan\\Desktop\\4. Semester\\Git\\compiler\\src\\TestProgram.txt");
 
         PrettyPrint.Init(InitAST(RunParser(charStream)));
     }
->>>>>>> c03ee011351a28378eaf53d5b6272516664f433f
 
     private static FinalGrammarParser InitParser(org.antlr.v4.runtime.CharStream charStream){
         FinalGrammarLexer lexer = new FinalGrammarLexer(charStream);
