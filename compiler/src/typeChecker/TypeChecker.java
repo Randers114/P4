9fa304fe7b6adef1e66f84af413475d39d64b432
NikolajLJO <nljarr15@student.aau.dk>
2017-04-11 15:56:03 +0200

    DclTypeCheck should work as intended, but will mostly like be chnaged to accomodate generic types while making the methods + everything still to be done easier if DOUBLE DISPATCH gets implementet / researched

--- compiler/src/typeChecker/TypeChecker.java
+++ compiler/src/typeChecker/TypeChecker.java
@@ -21,9 +21,9 @@ public class TypeChecker
 	public void TypeCheckChildren (Node node, SymbolTable ST)
 	{
 		if (node instanceof MethodNode)
-			TypeCheckMethod(node);
+			TypeCheckMethod(node, ST);
 		else if (node instanceof BlockNode)
-			TypeCheckBlock(node);
+			TypeCheckBlock(node, ST);
 		else if (node instanceof BodyNode)
 			TypeCheckBody(node, ST);
 		else if (node instanceof PlusNode)
@@ -38,22 +38,22 @@ public class TypeChecker
 			return false;
 	}
 
-	public void TypeCheckMethod(Node node)
+	public void TypeCheckMethod(Node node, SymbolTable ST)
 	{
 		if(SameType(((MethodNode) node).returnval, ((MethodNode) node).type))
 		{
 			System.out.println("method " + ((MethodNode) node).id + "is type correct");
-			TypeCheckChildren(((MethodNode) node).block);
+			TypeCheckChildren(((MethodNode) node).block, ST);
 		}
 		else
 			System.out.println("method " + ((MethodNode) node).id + "is NOT type correct");
 	}
 
-	public void TypeCheckBlock(Node node)
+	public void TypeCheckBlock(Node node, SymbolTable ST)
 	{
 		for (Node child: (((BlockNode)node).ChildrenList))
 		{
-			TypeCheckChildren(child);
+			TypeCheckChildren(child, ST);
 		}
 	}
 
@@ -62,29 +62,37 @@ public class TypeChecker
 		if (node instanceof StmtNode)
 			TypeCheckStmtNode(node);
 		else if (node instanceof DclNode)
-			TypeCheckDclNode(node, ST);
+			TypeCheckDclNode((DclNode)node, ST);
 		else if (node instanceof CallNode)
 			TypeCheckCallNode(node);
 	}
 
-	public void TypeCheckDclNode(Node node, SymbolTable ST)
+	public void TypeCheckDclNode(DclNode node, SymbolTable ST)
 	{
-		if (IsOpNode(((DclNode)node).right))
-			TypeCheckOpNode((((DclNode)node).right), ST);
-		else if (((DclNode)node).right instanceof NumberNode && ((TypesNode)(((DclNode)node).left)).type == "number")
-			print("dcl *number " + ((DclNode)node).middle + " = " + ((NumberNode)((DclNode)node).right).value + " evalutaed correctly");
+		if (IsOpNode((node).right) && ((TypesNode)((node).left)).type == "number")
+			TypeCheckOpNode((((node).right), ST);
+		else if (((node).right) instanceof BoolValOpNode || (((node).right) instanceof BoolOpNode && ((TypesNode)((node).left)).type == "boolean"))
+			TypeCheckBoolValOpNode(((node).right), ST);
+		else if ((node).right instanceof NumberNode && ((TypesNode)((node).left)).type == "number")
+			print("dcl number " + (node).middle + " = " + ((NumberNode)(node).right).value + " evalutaed correctly");
+		else if((node).right instanceof BoolNode && ((TypesNode) ((node).left)).type == "bool")
+			print("dcl bool " + (node).middle + " = " + ((BoolNode)(node).right).aBoolean + " evaluted correctly");
 	}
 
 	public void TypeCheckOpNode(Node node, SymbolTable ST)
 	{
 		if (IsOpNode(((OpNode)node).left))
 			TypeCheckOpNode(((OpNode)node).left, ST);
-		else if (IsOpNode(((OpNode)node).right))
+		if (IsOpNode(((OpNode)node).right))
 			TypeCheckOpNode(((OpNode)node).right, ST);
-		else if (((OpNode)node).left instanceof  NumberNode || ST.LookUp((((IdentifierNode)((OpNode)node).left).name))  && ((OpNode)node).right instanceof NumberNode || ST.LookUp((((IdentifierNode)((OpNode)node).right).name)) )
+		if (((OpNode)node).left instanceof  NumberNode || ST.LookUp((((IdentifierNode)((OpNode)node).left).name))  && ((OpNode)node).right instanceof NumberNode || ST.LookUp((((IdentifierNode)((OpNode)node).right).name)) )
 			print("Expr eval correctly");
 		else
 			print("Expr cold not svalutae correctly");
+
+	}
+	public void TypeCheckBoolValOpNode(Node node, SymbolTable ST)
+	{
 
 	}
 
@@ -108,8 +116,19 @@ public class TypeChecker
 		if (	((DclNode)node).left instanceof PlusNode ||
 				((DclNode)node).left instanceof MinusNode ||
 				((DclNode)node).left instanceof TimesNode ||
+				((DclNode)node).left instanceof DivideNode)
+			return true;
+		else
+			return false;
+	}
+	public boolean IsOpNodeExtended(Node node)
+	{
+		if (	((DclNode)node).left instanceof PlusNode ||
+				((DclNode)node).left instanceof MinusNode ||
+				((DclNode)node).left instanceof TimesNode ||
 				((DclNode)node).left instanceof DivideNode ||
-				((DclNode)node).left instanceof BoolOpNode)
+				((DclNode)node).left instanceof BoolOpNode ||
+				((DclNode)node).left instanceof BoolValOpNode)
 			return true;
 		else
 			return false;
