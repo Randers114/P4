c66441995508dda5a0d6f9e67850524353dd4309
Lasse Kristensen <lassekristensen@h471.aau1x-2.klient.slv.site.aau.dk>
2017-04-24 13:29:20 +0200

    Create c file

--- compiler/src/codeGenerator/CodeGenerator.java
+++ compiler/src/codeGenerator/CodeGenerator.java
@@ -1,30 +1,51 @@
 package codeGenerator;
 
-import abstractSyntaxTree.nodes.*;
-import symbolTable.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 
 /**
  * Created by lassekristensen on 20/04/2017.
  */
 
 public class CodeGenerator {
+    public void openfile() {
+        try {
+            File file = new File(("newfile.C"));
 
-    public boolean test (Node node1){
-        int i = 1;
-        if(i == 1)
-        {
-           System.out.println("Fuck Simon");
+            if (file.createNewFile()) {
+                System.out.println("File is created!");
+            } else {
+                System.out.println("File already exists.");
+            }
         }
-        else
-        {
-            System.out.println("Simon sutter");
+        catch (IOException exception) {
+            exception.printStackTrace();
         }
 
-        return false;
+        try {
+            PrintWriter writer = new PrintWriter("newfile.C", "UTF-8");
+            writer.println("#include <stdio.h>");
+
+            writer.close();
+        }
+        catch (IOException exception) {
+            System.out.println(exception);
+        }
     }
 
-    public boolean test2(SymbolTable st){
-        return false;
+   /* public int treewalk(Node node)
+    {
+        int result = 0;
+
+
+        return result;
+    }*/
+
+
     }
 
-}
+
+
+
