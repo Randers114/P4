eb06121ae89079c4fa603d2b77be68d531454921
Randers114 <srande15@student.aau.dk>
2017-04-25 14:10:30 +0200

    commented accept in node

--- compiler/src/abstractSyntaxTree/nodes/Node.java
+++ compiler/src/abstractSyntaxTree/nodes/Node.java
@@ -1,8 +1,12 @@
 package abstractSyntaxTree.nodes;
 
+import sun.reflect.generics.visitor.Visitor;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Node{
     public List<Node> ChildrenList = new ArrayList<>();
+
+    //public abstract void Accept(Visitor visitor);
 }
