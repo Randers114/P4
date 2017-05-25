669b5d61d1f076533bb9be454a0f367d10dfc500
Randers114 <srande15@student.aau.dk>
2017-05-25 15:53:56 +0200

    testprogram change

--- compiler/Mindstorms.c
+++ compiler/Mindstorms.c
@@ -1,84 +1,10 @@
-#pragma config(Motor, motorA, motorLeft, tmotorEV3_Large)
-#pragma config(Motor, motorB, motorRight, tmotorEV3_Large)
-#pragma config(Motor, motorC, motorFront, tmotorEV3_Large)
-#pragma config(Sensor, S1, tSensorTop, sensorEV3_Touch)
-#pragma config(Sensor, S2, tSensorFront, sensorEV3_Touch)
-#pragma config(Sensor, S3, uSensorLeft, sensorEV3_Ultrasonic)
-#pragma config(Sensor, S4, uSensorRight, sensorEV3_Ultrasonic)
+#pragma config(Motor, motorA, M1, tmotorEV3_Large)
+#pragma config(Motor, motorB, M2, tmotorEV3_Large)
 
-void TurnLeft(float speed); 
-void TurnRight(float speed); 
 
 task main() 
 {
-	float b = 10.0;
-	float speed = 75.0;
-	float backSpeed = 25.0;
-	float turningSpeed = 35.0;
-	bool isRunning = false;
-	while (! (isRunning))
-	{
-		if(getTouchValue(S1))
-		{
-			isRunning = true; 
-		}
-		sleep(100.0);
-	}
-	while (isRunning)
-	{
-		motor[motorA] = speed;
-		motor[motorB] = speed;
-		if(getTouchValue(S2))
-		{
-			stopMotor(motorA);
-			stopMotor(motorB);
-			motor[motorA] = - (backSpeed);
-			motor[motorB] = - (backSpeed);
- 			sleep(2.0 * 1000);
-			stopMotor(motorA);
-			stopMotor(motorB);
-			if(getUSDistance(S3) > getUSDistance(S4))
-			{
-				TurnLeft(turningSpeed);
-			}
-			else if(getUSDistance(S3) < getUSDistance(S4))
-			{
-				TurnRight(turningSpeed);
-			}
-			else
-			{
-				TurnLeft(turningSpeed);
-			} 
-			for ( int i = 1.0 ; i <20.0 ; i++ )
-			{ 
-				if(getTouchValue(S1))
-				{
-					isRunning = false; 
-				}
-				sleep(100.0);
-			}
-		}
-	}
+	float a = 9.0;
+	float b;
+	bool ghj = 1341234.0 < a;
 } 
-
-void TurnLeft(float speed)
-{
-	motor[motorA] = - (speed);
-	motor[motorB] = - (speed);
-	motor[motorC] = - (speed);
- 	sleep(3.0 * 1000);
-	stopMotor(motorA);
-	stopMotor(motorB);
-	stopMotor(motorC);
-}
-
-void TurnRight(float speed)
-{
-	motor[motorA] = - (speed);
-	motor[motorC] = - (speed);
- 	sleep(3.0 * 1000);
-	stopMotor(motorA);
-	stopMotor(motorC);
-	motor[motorA] = speed;
-	motor[motorC] = speed;
-}
