2c1a1da972f838307354c27e540b843e0f87df08
Randers114 <srande15@student.aau.dk>
2017-05-28 22:18:25 +0200

    last changes

--- compiler/Mindstorms.c
+++ compiler/Mindstorms.c
@@ -1,14 +1,41 @@
+#pragma config(Motor, motorB, motorLeft, tmotorEV3_Large)
+#pragma config(Motor, motorC, motorRight, tmotorEV3_Large)
+#pragma config(Sensor, S4, uSensor, sensorEV3_Ultrasonic)
 
+void TurnInPlace(float speed); 
 
 task main() 
 {
-	bool b = true;
-	bool a;
-	a = b; 
-	if(a)
+	float speed = 75.0;
+	float dangerSpeed = 35.0;
+	float minimumDisantance = 15.0;
+	float dangerDistance = 35.0;
+	float maxTurns = 10.0;
+	float actualTurns = 0.0;
+	while (actualTurns < maxTurns)
 	{
-		if(b)
+		while (getUSDistance(S4) < dangerDistance)
 		{
+			motor[motorB] = speed;
+			motor[motorC] = speed;
+		}
+		motor[motorB] = dangerSpeed;
+		motor[motorC] = dangerSpeed;
+		if(getUSDistance(S4) > minimumDisantance)
+		{
+			stopMotor(motorB);
+			stopMotor(motorC);
+			TurnInPlace(dangerSpeed);
+			actualTurns = actualTurns + 1.0; 
 		}
 	}
 } 
+
+void TurnInPlace(float speed)
+{
+	motor[motorB] = speed;
+	motor[motorC] = ((- 100.0) / 100) * (speed);
+ 	sleep(3.0 * 1000);
+	stopMotor(motorB);
+	stopMotor(motorC);
+}
